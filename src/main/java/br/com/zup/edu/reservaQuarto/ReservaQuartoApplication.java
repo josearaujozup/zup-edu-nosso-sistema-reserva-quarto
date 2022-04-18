7c5e23afd0dd2eb41cc71893b4496acbefd42c24
Denes <jose.araujo@zup.com.br>
2022-04-18 15:58:03 -0300

    commit inicial

---
+++ src/main/java/br/com/zup/edu/reservaQuarto/ReservaQuartoApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.reservaQuarto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaQuartoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaQuartoApplication.class, args);
+	}
+
+}
